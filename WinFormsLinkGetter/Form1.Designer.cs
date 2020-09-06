5d2d40f187809f5aafeadc99d31171f314329774
Guy Barker (CELA) <guybark@microsoft.com>
2020-09-06 16:09:24 -0700

    Add project files.

---
+++ WinFormsLinkGetter/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace WinFormsLinkGetter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.labelInstructions = new System.Windows.Forms.Label();
+            this.labelPage = new System.Windows.Forms.Label();
+            this.buttonExport = new System.Windows.Forms.Button();
+            this.labelURL = new System.Windows.Forms.Label();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.webBrowserPage = new System.Windows.Forms.WebBrowser();
+            this.buttonLoadURL = new System.Windows.Forms.Button();
+            this.buttonFindLinks = new System.Windows.Forms.Button();
+            this.checkedListBoxLinks = new System.Windows.Forms.CheckedListBox();
+            this.labelLinkCount = new System.Windows.Forms.Label();
+            this.buttonSelectAll = new System.Windows.Forms.Button();
+            this.buttonDeselectAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.Location = new System.Drawing.Point(599, 696);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(148, 56);
+            this.buttonClose.TabIndex = 11;
+            this.buttonClose.Text = "&Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // labelInstructions
+            // 
+            this.labelInstructions.AutoSize = true;
+            this.labelInstructions.Location = new System.Drawing.Point(16, 9);
+            this.labelInstructions.Name = "labelInstructions";
+            this.labelInstructions.Size = new System.Drawing.Size(737, 120);
+            this.labelInstructions.TabIndex = 0;
+            this.labelInstructions.Text = resources.GetString("labelInstructions.Text");
+            // 
+            // labelPage
+            // 
+            this.labelPage.AutoSize = true;
+            this.labelPage.Location = new System.Drawing.Point(117, 142);
+            this.labelPage.Name = "labelPage";
+            this.labelPage.Size = new System.Drawing.Size(0, 20);
+            this.labelPage.TabIndex = 0;
+            // 
+            // buttonExport
+            // 
+            this.buttonExport.Location = new System.Drawing.Point(20, 696);
+            this.buttonExport.Name = "buttonExport";
+            this.buttonExport.Size = new System.Drawing.Size(148, 56);
+            this.buttonExport.TabIndex = 10;
+            this.buttonExport.Text = "&Export links";
+            this.buttonExport.UseVisualStyleBackColor = true;
+            this.buttonExport.Click += new System.EventHandler(this.buttonExport_Click);
+            // 
+            // labelURL
+            // 
+            this.labelURL.AutoSize = true;
+            this.labelURL.Location = new System.Drawing.Point(16, 159);
+            this.labelURL.Name = "labelURL";
+            this.labelURL.Size = new System.Drawing.Size(46, 20);
+            this.labelURL.TabIndex = 1;
+            this.labelURL.Text = "&URL:";
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(68, 156);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(659, 26);
+            this.textBoxURL.TabIndex = 2;
+            // 
+            // webBrowserPage
+            // 
+            this.webBrowserPage.Location = new System.Drawing.Point(20, 270);
+            this.webBrowserPage.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserPage.Name = "webBrowserPage";
+            this.webBrowserPage.ScriptErrorsSuppressed = true;
+            this.webBrowserPage.Size = new System.Drawing.Size(727, 135);
+            this.webBrowserPage.TabIndex = 4;
+            // 
+            // buttonLoadURL
+            // 
+            this.buttonLoadURL.Location = new System.Drawing.Point(20, 197);
+            this.buttonLoadURL.Name = "buttonLoadURL";
+            this.buttonLoadURL.Size = new System.Drawing.Size(148, 56);
+            this.buttonLoadURL.TabIndex = 3;
+            this.buttonLoadURL.Text = "&Load URL";
+            this.buttonLoadURL.UseVisualStyleBackColor = true;
+            this.buttonLoadURL.Click += new System.EventHandler(this.buttonLoadURL_Click);
+            // 
+            // buttonFindLinks
+            // 
+            this.buttonFindLinks.Location = new System.Drawing.Point(20, 426);
+            this.buttonFindLinks.Name = "buttonFindLinks";
+            this.buttonFindLinks.Size = new System.Drawing.Size(148, 56);
+            this.buttonFindLinks.TabIndex = 5;
+            this.buttonFindLinks.Text = "&Find links";
+            this.buttonFindLinks.UseVisualStyleBackColor = true;
+            this.buttonFindLinks.Click += new System.EventHandler(this.buttonFindLinks_Click);
+            // 
+            // checkedListBoxLinks
+            // 
+            this.checkedListBoxLinks.FormattingEnabled = true;
+            this.checkedListBoxLinks.Location = new System.Drawing.Point(20, 501);
+            this.checkedListBoxLinks.Name = "checkedListBoxLinks";
+            this.checkedListBoxLinks.Size = new System.Drawing.Size(733, 119);
+            this.checkedListBoxLinks.TabIndex = 7;
+            // 
+            // labelLinkCount
+            // 
+            this.labelLinkCount.AutoSize = true;
+            this.labelLinkCount.Location = new System.Drawing.Point(187, 445);
+            this.labelLinkCount.Name = "labelLinkCount";
+            this.labelLinkCount.Size = new System.Drawing.Size(0, 20);
+            this.labelLinkCount.TabIndex = 6;
+            // 
+            // buttonSelectAll
+            // 
+            this.buttonSelectAll.Location = new System.Drawing.Point(20, 639);
+            this.buttonSelectAll.Name = "buttonSelectAll";
+            this.buttonSelectAll.Size = new System.Drawing.Size(148, 41);
+            this.buttonSelectAll.TabIndex = 8;
+            this.buttonSelectAll.Text = "&Select all";
+            this.buttonSelectAll.UseVisualStyleBackColor = true;
+            this.buttonSelectAll.Click += new System.EventHandler(this.buttonSelectAll_Click);
+            // 
+            // buttonDeselectAll
+            // 
+            this.buttonDeselectAll.Location = new System.Drawing.Point(174, 639);
+            this.buttonDeselectAll.Name = "buttonDeselectAll";
+            this.buttonDeselectAll.Size = new System.Drawing.Size(148, 41);
+            this.buttonDeselectAll.TabIndex = 9;
+            this.buttonDeselectAll.Text = "&Deselect all";
+            this.buttonDeselectAll.UseVisualStyleBackColor = true;
+            this.buttonDeselectAll.Click += new System.EventHandler(this.buttonDeselectAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 771);
+            this.Controls.Add(this.labelInstructions);
+            this.Controls.Add(this.labelURL);
+            this.Controls.Add(this.textBoxURL);
+            this.Controls.Add(this.buttonLoadURL);
+            this.Controls.Add(this.webBrowserPage);
+            this.Controls.Add(this.buttonFindLinks);
+            this.Controls.Add(this.labelLinkCount);
+            this.Controls.Add(this.checkedListBoxLinks);
+            this.Controls.Add(this.buttonSelectAll);
+            this.Controls.Add(this.buttonDeselectAll);
+            this.Controls.Add(this.buttonExport);
+            this.Controls.Add(this.buttonClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Link Getter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Label labelInstructions;
+        private System.Windows.Forms.Label labelPage;
+        private System.Windows.Forms.Button buttonExport;
+        private System.Windows.Forms.Label labelURL;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.WebBrowser webBrowserPage;
+        private System.Windows.Forms.Button buttonLoadURL;
+        private System.Windows.Forms.Button buttonFindLinks;
+        private System.Windows.Forms.CheckedListBox checkedListBoxLinks;
+        private System.Windows.Forms.Label labelLinkCount;
+        private System.Windows.Forms.Button buttonSelectAll;
+        private System.Windows.Forms.Button buttonDeselectAll;
+    }
+}
+
---
+++ WinFormsLinkGetter/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Windows.Forms;
+using UIAInterop;
+
+//This app will look for links on a web page that reference the same page, and export those links to a file 
+//such that the links can be incorporated into Q&A pairs for an Azure QnAMaker knowledge base. 
+
+//For example, say the link was "http://mysite.com/help#birds". The QnAMaker knowledge base will end up
+//with a Q&&A pair whose question is "birds", and whose answer is "For details on birds, please visit birds",
+//where the second instance of birds is the links to http://mysite.com/help#birds.
+
+// The app is publicly available at https://github.com/guybark/WinFormsLinkGetter.
+
+namespace WinFormsLinkGetter
+{
+    public partial class Form1 : Form
+    {
+        private CUIAutomation8 automation;
+
+        // These UIA values are taken from:
+        // https://docs.microsoft.com/en-us/windows/win32/winauto/uiauto-automation-element-propids
+        // https://docs.microsoft.com/en-us/windows/win32/winauto/uiauto-control-pattern-propids
+        // https://docs.microsoft.com/en-us/windows/win32/winauto/uiauto-controltype-ids
+        // https://docs.microsoft.com/en-us/windows/win32/winauto/uiauto-controlpattern-ids
+
+        private const int patternIdValue = 10002;
+        private const int propertyIdControlType = 30003;
+        private const int propertyIdName = 30005;
+        private const int propertyIdValueValue = 30045;
+        private const int controlTypeIdHyperlink = 50005;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            automation = new CUIAutomation8();
+
+            // Let's get the ball rolling with a W3C web page.
+            textBoxURL.Text = "https://w3c.github.io/aria-practices";
+
+            // TODO: Investigate why the empty WebBrowser seems to be a keyboard trap.
+            // For now, take it out of the tab order it doesn't have a page loaded.
+            webBrowserPage.TabStop = false;
+
+            // Make sure all controls have appropriate accessible names.
+            checkedListBoxLinks.AccessibleName = "Links found"; // TODO: Remember to localized accessible names.
+            webBrowserPage.AccessibleName = "Web page with links";
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Step 1: Load up the target page in the app.
+        private void buttonLoadURL_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxURL.Text))
+            {
+                MessageBox.Show(this,
+                    "Please provide an URL for the web page of interest.",
+                    "Link Getter",
+                    MessageBoxButtons.OK);
+
+                return;
+            }
+
+            try
+            {
+                webBrowserPage.Navigate(new Uri(textBoxURL.Text));
+
+                webBrowserPage.TabStop = true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(this,
+                    "Sorry, I wasn't able to navigate to that URL.",
+                    "Link Getter",
+                    MessageBoxButtons.OK);
+            }
+        }
+
+        // Step 2: Find all the links of interest on the loaded page.
+        private void buttonFindLinks_Click(object sender, EventArgs e)
+        {
+            checkedListBoxLinks.Items.Clear();
+
+            if (webBrowserPage.Url == null)
+            {
+                MessageBox.Show(this,
+                    "Please load the web page of interest.",
+                    "Link Getter",
+                    MessageBoxButtons.OK);
+
+                labelLinkCount.Text = "No links found.";
+
+                return;
+            }
+
+            // Get the UIA element for the webBrowser with the page of interest loaded.
+            IUIAutomationElement elementBrowser = 
+                automation.ElementFromHandle(webBrowserPage.Handle);
+
+            // Build up a cache request for all the data we need, to reduce the time it 
+            // takes to access the link data once with have the collection of links.
+            IUIAutomationCacheRequest cacheRequest = automation.CreateCacheRequest();
+
+            cacheRequest.AddProperty(propertyIdName);
+
+            cacheRequest.AddPattern(patternIdValue);
+            cacheRequest.AddProperty(propertyIdValueValue);
+
+            // Assume the links have names as expected, and we don't need to 
+            // search children of the links for names.
+            cacheRequest.TreeScope = TreeScope.TreeScope_Element;
+
+            // We want the collection of all links on the page.
+            IUIAutomationCondition conditionControlView = automation.ControlViewCondition;
+            IUIAutomationCondition conditionHyperlink = 
+                automation.CreatePropertyCondition(
+                    propertyIdControlType, controlTypeIdHyperlink);
+
+            IUIAutomationCondition finalCondition = automation.CreateAndCondition(
+                conditionControlView, conditionHyperlink);
+
+            // TODO: Call FindAllBuildCache() in a background thread in case it takes 
+            // a while. As it is, the app UI's going to freeze.
+
+            // Now get the collection of links.
+            IUIAutomationElementArray elementArray = elementBrowser.FindAllBuildCache(
+                TreeScope.TreeScope_Descendants, finalCondition, cacheRequest);
+            if (elementArray != null)
+            {
+                // Process each returned hyperlink element.
+                for (int idxLink = 0; idxLink < elementArray.Length; ++idxLink)
+                {
+                    IUIAutomationElement elementLink = elementArray.GetElement(idxLink);
+
+                    // Despite the fact that we've got the names of the UIA links, don't
+                    // use that information here. Perhaps we will use it in the future.
+
+                    IUIAutomationValuePattern valuePattern = 
+                        (IUIAutomationValuePattern)elementLink.GetCurrentPattern(
+                        patternIdValue);
+                    if (valuePattern != null)
+                    {
+                        // We're only interested in references the page makes to itself.
+                        string strValueLink = valuePattern.CachedValue;
+                        var index = strValueLink.IndexOf('#');
+                        if ((index > 0) && strValueLink.StartsWith(textBoxURL.Text))
+                        {
+                            checkedListBoxLinks.Items.Add(strValueLink);
+                        }
+                    }
+                }
+            }
+
+            // Let's assume we'll want most of the links found.
+            SetLinkCheckedState(true);
+
+            labelLinkCount.Text = "Count of links found: " + 
+                checkedListBoxLinks.Items.Count;
+        }
+
+        // Step 3: Export the link info to a tsv file.
+        private void buttonExport_Click(object sender, EventArgs e)
+        {
+            if (checkedListBoxLinks.Items.Count == 0)
+            {
+                MessageBox.Show(this,
+                    "Please get some links to export first.",
+                    "Link Getter",
+                    MessageBoxButtons.OK);
+
+                return;
+            }
+
+            // Get the target file to contain the info.
+            var dlg = new SaveFileDialog();
+            dlg.Title = "Export links to file";
+            dlg.Filter = "Tab separated values|*.tsv";
+
+            var result = dlg.ShowDialog(this);
+            if(result == DialogResult.OK)
+            {
+                // Prepare the file for all the link data.
+                using (System.IO.StreamWriter file =
+                    new System.IO.StreamWriter(dlg.FileName))
+                {
+                    // The first line in the file must match the expectations of QnAMaker.
+                    string firstLine = "Question\tAnswer";
+                    file.WriteLine(firstLine);
+
+                    // Process each link we found.
+                    for (int index = 0; index < checkedListBoxLinks.Items.Count; ++index)
+                    {
+                        if (checkedListBoxLinks.GetItemChecked(index))
+                        {
+                            string strValueLink = checkedListBoxLinks.Items[index].ToString();
+
+                            var termIndex = strValueLink.IndexOf('#');
+                            if (termIndex > 0)
+                            {
+                                string term = strValueLink.Substring(termIndex + 1);
+
+                                string answer = "For details on " + term +
+                                    ", please visit [" + term + "](" +
+                                    strValueLink + ")";
+
+                                file.WriteLine(term + "\t" + answer);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void buttonSelectAll_Click(object sender, EventArgs e)
+        {
+            SetLinkCheckedState(true);
+        }
+
+        private void buttonDeselectAll_Click(object sender, EventArgs e)
+        {
+            SetLinkCheckedState(false);
+        }
+
+        private void SetLinkCheckedState(bool setChecked)
+        {
+            for (int i = 0; i < checkedListBoxLinks.Items.Count; i++)
+            {
+                checkedListBoxLinks.SetItemChecked(i, setChecked);
+            }
+        }
+    }
+}
